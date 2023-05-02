77e98777495d7a72b5d1d4589946b84f9ab80072
igormich <igormich88@gmail.com>
2023-05-03 00:42:18 +0500

    I have added optimized version of Kotlin code that work faster than Cpp (at least in my configuration). Major change is move Complex object out of cycle and reuse it. Results on the emulator (Kotlin - 25709ms, optimized Kotlin - 1296ms, C++ - 4926ms) Results on POCO M3 Pro(Kotlin - 17681ms, optimized Kotlin - 1085ms, C++ - 1708ms)

---
+++ app/src/main/java/ru/tusur/nativevskotlin1/model2/Complex.kt
@@ -0,0 +1,18 @@
+package ru.tusur.nativevskotlin1.model2
+
+data class Complex(var real: Double = 0.0, var im: Double = 0.0) {
+    // plus operator
+    operator fun plus(b: Complex): Complex {
+        return Complex(real + b.real, im + b.im)
+    }
+
+    fun squareSelf() {
+        val r = real * real - im * im
+        val im = 2 * real * im
+        real = r
+        this.im = im
+    }
+
+    // square of number module
+    fun sqrAbs() = real * real + im * im
+}
